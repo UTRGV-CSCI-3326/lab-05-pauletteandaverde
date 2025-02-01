53a6b741ca07f7395503a6b3416e2c15fb374849
pauletteandaverde <paulette.andaverde01@utrgv.edu>
2025-02-01 19:00:43 +0000

    First commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+public class Input {
+
+    public static void main(String[] args){
+        // Setting up object for Scanner
+        Scanner scanner = new Scanner(System.in);
+        // Varibles
+        String name;
+        int age;
+        double weight;
+        boolean smoker;
+
+        // Assigning inputs
+        System.out.print("Full Name: ");
+        name = scanner.nextLine();
+        System.out.print("Age: ");
+        age = scanner.nextInt();
+        System.out.print("Weight: ");
+        weight = scanner.nextDouble();
+        System.out.print("Are you a smoker? (Write 'true' or 'false'): ");
+        smoker = scanner.nextBoolean(); 
+
+        // Outputting info
+        System.out.println("Patient's Full Name: " + name);
+        System.out.println("Patient's Age: " + age);
+        System.out.println("Patient's Weight: " + weight);
+        System.out.println("Patient is a smoker: " + smoker);
+
+        scanner.close();
+    }
+}
